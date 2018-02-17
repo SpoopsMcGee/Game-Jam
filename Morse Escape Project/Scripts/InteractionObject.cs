7b140eeeb1c5f4d7983428f6c05dd0cfddd40a1e
SpoopsMcGee <35858915+SpoopsMcGee@users.noreply.github.com>
2018-02-17 15:21:05 -0500

    .

---
+++ Morse Escape Project/Scripts/InteractionObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour {
+
+    public void DoInteraction()
+
+    {
+        // checked to see if items have audio file
+        //(
+
+    }
+}
